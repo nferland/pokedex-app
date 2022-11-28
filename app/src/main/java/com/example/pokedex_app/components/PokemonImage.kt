81ebb332fcb7d081bfbd562e21deef8906a166a1
Nicholas Ferland <nicholas.ferland03@gmail.com>
2022-11-28 11:08:40 -0500

    refactor: Rename round image, and change to normal image

--- app/src/main/java/com/example/pokedex_app/components/PokemonImage.kt
+++ app/src/main/java/com/example/pokedex_app/components/PokemonImage.kt
@@ -1,13 +1,10 @@
 package com.example.myapplication.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
@@ -17,13 +14,12 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.pokedex_app.R
 
 @Composable
-fun RoundPokemonImage(size: Int, imageUrl: String?) {
+fun PokemonImage(size: Int, imageUrl: String?) {
     Column() {
         if (imageUrl == null){
             Image(
                 painter = painterResource(id = R.drawable.missing_no),
-                modifier = Modifier.size(size.dp).clip(CircleShape)                       // clip to the circle shape
-                    .border(2.dp, Color.Gray, CircleShape),
+                modifier = Modifier.size(size.dp),
                 contentDescription = "Account",
                 colorFilter = ColorFilter.tint(color = Color.LightGray),
                 contentScale = ContentScale.Crop,            // crop the image if it's not a square
@@ -31,10 +27,9 @@ fun RoundPokemonImage(size: Int, imageUrl: String?) {
         }else{
             Image(
                 painter = rememberAsyncImagePainter(imageUrl),
-                modifier = Modifier.size(size.dp).clip(CircleShape)                       // clip to the circle shape
-                    .border(2.dp, Color.Gray, CircleShape),
+                modifier = Modifier.size(size.dp),
                 contentScale = ContentScale.Crop,
-                contentDescription = "Account",
+                contentDescription = "Pokemon",
             )
         }
     }
