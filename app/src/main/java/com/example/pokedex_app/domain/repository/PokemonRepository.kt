20f7ae5df2b107d8def89a0ef8b011f41da446ba
Nicholas Ferland <nicholas.ferland03@gmail.com>
2022-11-28 11:09:30 -0500

    refactor: delete dead code

--- app/src/main/java/com/example/pokedex_app/domain/repository/PokemonRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pokedex_app.domain.repository
-
-import com.example.pokedex_app.data.api.dto.pokemon.PokemonDetails
-
-interface PokemonRepository {
-    suspend fun loadPokemons(): List<PokemonDetails>?
-}
